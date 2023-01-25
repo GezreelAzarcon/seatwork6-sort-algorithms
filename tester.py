02bf0efa42c8c180b7292980483ec9adf5fda035
GezreelAzarcon <gezreelwazarcon@gmail.com>
2023-01-25 16:10:28 +0800

    Tester File, Sorting Functions Called Here.

--- tester.py
+++ tester.py
@@ -4,8 +4,20 @@ from sortAlgorithms import insertionSort
 from sortAlgorithms import mergeSort
 from sortAlgorithms import quickSort
 
-myArray = [45, 55, 57, 17, 23, 19, 73, 92, 60, 26]
+myArray1 = [45, 55, 57, 17, 23, 19, 73, 92, 60, 26]
+myArray2 = [45, 55, 57, 17, 23, 19, 73, 92, 60, 26]
+myArray3 = [45, 55, 57, 17, 23, 19, 73, 92, 60, 26]
+myArray4 = [45, 55, 57, 17, 23, 19, 73, 92, 60, 26]
+myArray5 = [45, 55, 57, 17, 23, 19, 73, 92, 60, 26]
 
-print(quickSort(myArray))
+selectionSort(myArray1)
+bubbleSort(myArray2)
+insertionSort(myArray3)
+mergeSort(myArray4)
+myArray5 = (quickSort(myArray5))
 
-
+print(myArray1)
+print(myArray2)
+print(myArray3)
+print(myArray4)
+print(myArray5)
